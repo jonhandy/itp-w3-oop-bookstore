3d233ca715a46cb82e002d6cc83527a41996c7e4
Jon Handy <jonhandy@gmail.com>
2017-06-16 01:45:17 +0000

    First Commit, yay!

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,54 @@
 class Bookstore(object):
-    pass
-
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+      
+    def get_books(self):
+        return self.books
+        
+    def add_book(self, book):
+        self.books.append(book)
+        
+    def search_books(self, title=None, author=None):
+        rtn_list = []
+        if not author and not title:
+            print("oh no")
+        elif author == None:
+            for book in self.books:
+                if title.lower() in book.title.lower():
+                    rtn_list.append(book)
+        elif title == None:
+            for book in self.books:
+                if author.name.lower() in book.author.name.lower():
+                    rtn_list.append(book)
+        else:
+            for book in self.books:
+                if title.lower() in book.title.lower() and author.name.lower() in book.author.name.lower():
+                    rtn_list.append(book)
+                    
+        return rtn_list
+    
+        
 
 class Author(object):
-    pass
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.titles = []
+        
+    def get_books(self):
+        return self.titles
+        
+        
 
 
 class Book(object):
-    pass
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        author.titles.append(self)
+        
+        
+        
+        
+#my_bookstore.books[0].author.name
